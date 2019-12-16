cb8a43808de7f755d558afd01c0d84e1656204e6
= <=>
2019-12-16 20:22:51 +0700

    database done

--- App_load/Model/TraineeMod.php
+++ App_load/Model/TraineeMod.php
@@ -1,8 +1,31 @@
 <?php
-
-
+include_once 'Trainee.php';
+include_once 'connDb.php';
 class TraineeMod
 {
+    public function __construct()
+    {
+    }
 
+    public function getAllTrainee(){
+        $connDb=new ConnDb();
+        $conn=$connDb->getConn();
+        $data=$conn->query("SELECT * FROM trainee");
+        //Id, Name, DateOfBirth, Address, Description, Lang, Toeic, UserName, Pass
+        $arrTrainee=array();
+        foreach ($data as $row){
+            $id=$row['Id'];
+            $name=$row['Name'];
+            $date=$row['DateOfBirth'];
+            $address=$row['Address'];
+            $des=$row['Description'];
+            $lang=$row['Lang'];
+            $toeic=$row['Toeic'];
+            $user=$row['Username'];
+            $pass=$row['Pass'];
+            array_push($arrTrainee,new Trainee($id,$name,$date,$address,$des,$lang,$toeic,$user,$pass));
+        }
+        return $arrTrainee;
+    }
 }
 ?>
--- App_load/Model/connDb.php
+++ App_load/Model/connDb.php
@@ -1,10 +1,20 @@
 <?php
-    $host = "localhost:3306";
-    $user = "root";
-    $pass = "";
-    $db = "fptproject";
-    $conn= new mysqli($host,$user,$pass,$db);
-    if(!$conn){
-    die("Connect fail"+mysqli_connect_errno());
+
+class ConnDb{
+    public function __construct()
+    {
     }
+    public function getConn(){
+        $host = "localhost:3306";
+        $user = "root";
+        $pass = "";
+        $db = "fptproject";
+        $conn= new mysqli($host,$user,$pass,$db);
+        if(!$conn){
+            die("Connect fail"+mysqli_connect_errno());
+        }
+        return $conn;
+    }
+}
+
 ?>
